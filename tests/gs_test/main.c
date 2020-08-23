5c72ea4e1f6da3934b68144a5902a7f50e2e27ef
Rafaël Kooi <48814281+RA-Kooi@users.noreply.github.com>
2020-08-23 21:10:11 +0200

    Fix /GS test in release

--- tests/gs_test/main.c
+++ tests/gs_test/main.c
@@ -1,7 +1,27 @@
 #include <stdlib.h>
 
+void use(char const volatile *c);
+void use(char const volatile *c)
+{
+	__LOTUSCRT_UNUSED(c);
+}
+
 int main(void)
 {
-	char buf[16];
-	buf[17] = 'a';
+	volatile char buf[16];
+	for(int i = 0; i < 20; ++i)
+		buf[i] = 'a';
+
+	use(buf);
+
+	// Make sure buf isn't optimized away in release mode.
+	// We obviously need it to be here to here to trigger /GS.
+#if defined(__LOTUSCRT_COMPILER_GNULIKE) \
+	|| defined(__LOTUSCRT_COMPILER_MSVC_CLANG)
+	__asm__ volatile("" : : "r,m"(buf) : "memory");
+#elif defined(__LOTUSCRT_COMPILER_MSVC_ONLY)
+	_ReadWriteBarrier();
+#endif
+
+	return 0;
 }
